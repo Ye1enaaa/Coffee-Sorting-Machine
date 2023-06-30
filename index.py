f22418305ec6da5bc6111d8f8314d327de288a1e
EErickson <avilasonson@gmail.com>
2023-06-30 14:36:15 +0800

    /actuators

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-import cv2  # Install opencv-python
+from keras.models import load_model
+import cv2
 import numpy as np
 
 # Disable scientific notation for clarity
@@ -11,11 +11,13 @@ model = load_model("keras_model.h5", compile=False)
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
 
-# CAMERA can be 0 or 1 based on default camera of your computer
+# CAMERA can be 0 or 1 based on the default camera of your computer
 camera = cv2.VideoCapture(2)
 
+bad_beans_count = 0  # Counter for consecutive bad beans
+
 while True:
-    # Grab the webcamera's image.
+    # Grab the web camera's image.
     ret, image = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
@@ -24,21 +26,29 @@ while True:
     # Show the image in a window
     cv2.imshow("Webcam Image", image)
 
-    # Make the image a numpy array and reshape it to the models input shape.
+    # Make the image a numpy array and reshape it to the model's input shape.
     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
     # Normalize the image array
     image = (image / 127.5) - 1
 
-    # Predicts the model
+    # Predict the model
     prediction = model.predict(image)
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
 
+    if class_name.strip() == "1 Bad":  # Check for "1 Bad" label
+        bad_beans_count += 1
+    else:
+        bad_beans_count = 0
+
     # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
+    print("Class:", class_name.strip()[2:])
     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    if bad_beans_count >= 5:
+        print("Actuators on")
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
