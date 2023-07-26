0b59d2e8dcae846fa0c7e3ee0c01110b8f21bdd5
Erickson Avila Dela Cruz <100282065+Ye1enaaa@users.noreply.github.com>
2023-07-26 22:48:09 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-from keras.models import load_model
+import tkinter as tk
 import cv2
 import numpy as np
+from keras.models import load_model
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -12,50 +13,75 @@ model = load_model("keras_model.h5", compile=False)
 class_names = open("labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on the default camera of your computer
-camera = cv2.VideoCapture(2)
+camera = cv2.VideoCapture(0)
 
 bad_beans_count = 0  # Counter for consecutive bad beans
 
-while True:
-    # Grab the web camera's image.
-    ret, image = camera.read()
+def start_scanning():
+    global bad_beans_count
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    while True:
+        # Grab the web camera's image.
+        ret, image = camera.read()
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-    # Make the image a numpy array and reshape it to the model's input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
+        # Make the image a numpy array and reshape it to the model's input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-    # Predict the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+        # Normalize the image array
+        image = (image / 127.5) - 1
 
-    if class_name.strip() == "1 Bad":  # Check for "1 Bad" label
-        bad_beans_count += 1
-    else:
-        bad_beans_count = 0
+        # Predict the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-    # Print prediction and confidence score
-    print("Class:", class_name.strip()[2:])
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        if class_name.strip() == "1 Bad":  # Check for "1 Bad" label
+            bad_beans_count += 1
+        else:
+            bad_beans_count = 0
 
-    if bad_beans_count >= 5:
-        print("Actuators on")
+        # Print prediction and confidence score
+        print("Class:", class_name.strip()[2:])
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
+        if bad_beans_count >= 15:
+            print("Actuators on")
+            
+            bad_beans_count = 0
+            # code for actuators
 
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
+        # Listen to the keyboard for presses.
+        keyboard_input = cv2.waitKey(1)
 
-camera.release()
-cv2.destroyAllWindows()
+        # 27 is the ASCII for the esc key on your keyboard.
+        if keyboard_input == 27:
+            break
+
+    camera.release()
+    cv2.destroyAllWindows()
+
+# Create the main application window
+root = tk.Tk()
+root.geometry("400x300")  # Set the window size (width x height)
+
+# Create a function to handle the button click event
+def button_click():
+    label.config(text="Scanning process started!")
+    start_scanning()
+
+# Create a label and a button in the window
+label = tk.Label(root, text="Click the button to start scanning!")
+label.pack(pady=20)
+button = tk.Button(root, text="Start Scanning", command=button_click)
+button.pack()
+
+# Start the Tkinter event loop
+root.mainloop()
+
