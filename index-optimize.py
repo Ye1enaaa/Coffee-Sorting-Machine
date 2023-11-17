3c2a55ba249c2997935d0df553855f6b7b33e340
EErickson <avilasonson@gmail.com>
2023-11-17 17:40:53 +0800

    update with threshold value

--- index-optimize.py
+++ index-optimize.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from keras.models import load_model
 import time
 import sqlite3
 
 # Constants
-MODEL_PATH = "keras_test_one.h5"
+MODEL_PATH = "keras_model.h5"
 LABELS_PATH = "labels.txt"
-BAD_THRESHOLD = 5
+BAD_THRESHOLD = 80  # Set the confidence threshold for "Bad" class
 GOOD_THRESHOLD = 5
 
 # Load the model and labels
@@ -16,9 +16,9 @@ model = load_model(MODEL_PATH, compile=False)
 class_names = [line.strip() for line in open(LABELS_PATH, "r")]
 
 # Set up GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+#GPIO.setup(18, GPIO.OUT)
 
 # Create the database and table
 with sqlite3.connect("bean_loggerist.db") as conn:
@@ -49,16 +49,18 @@ try:
         class_name = class_names[index]
         confidence_score = prediction[0][index]
 
-        if class_name == "1 Bad":
-            GPIO.output(18, GPIO.HIGH)
+        if class_name == "1 Bad" and confidence_score > BAD_THRESHOLD / 100:
+            # Trigger GPIO or take other action
+            print("Bad bean detected with high confidence!")
+            # GPIO.output(18, GPIO.HIGH)
             bad_consecutive_count += 1
             good_consecutive_count = 0
         elif class_name == "0 Good":
-            GPIO.output(18, GPIO.LOW)
+            #GPIO.output(18, GPIO.LOW)
             good_consecutive_count += 1
             bad_consecutive_count = 0
         else:
-            GPIO.output(18, GPIO.LOW)
+            #GPIO.output(18, GPIO.LOW)
             good_consecutive_count = 0
             bad_consecutive_count = 0
 
@@ -76,6 +78,6 @@ finally:
         cursor.execute('''INSERT INTO bean_counts_data(good, bad) VALUES (?, ?);''', (good_consecutive_count, bad_consecutive_count))
         conn.commit()
 
-    GPIO.cleanup()
+    #GPIO.cleanup()
     camera.release()
     cv2.destroyAllWindows()
