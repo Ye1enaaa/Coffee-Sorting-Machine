1ec55cc368091ea95d0a4c014f6077f8f66e35f1
EErickson <avilasonson@gmail.com>
2023-11-27 12:39:59 +0800

    update lightwieght code

--- Windows Test/lightweight.py
+++ Windows Test/lightweight.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from keras.models import load_model
 import time
 # Load the model
@@ -9,11 +9,11 @@ model = load_model("models/keras_mode11.h5", compile=False)
 # Load the labels for model 1
 class_names1 = open("labels.txt", "r").readlines()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+#GPIO.setup(18, GPIO.OUT)
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(2)
 
 try:
     while True:
@@ -35,16 +35,17 @@ try:
         confidence_score = prediction[0][index]
 
         if class_name == "1 Bad":
-            GPIO.output(18, GPIO.LOW)
+            print('hi')
+            #GPIO.output(18, GPIO.LOW)
         if class_name == "0 Good" or class_name == "2 Neutral":
-            GPIO.output(18, GPIO.HIGH)
-
+            #GPIO.output(18, GPIO.HIGH)
+            print(';')
         keyboard_input = cv2.waitKey(1)
 
         if keyboard_input == 27:
             break
         time.sleep(1)
 finally:
-    GPIO.cleanup()
+    #GPIO.cleanup()
     camera.release()
     cv2.destroyAllWindows()
